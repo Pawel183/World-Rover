68c8d1121c666171b88a401d9ecf52e92bea21b4
Paweł Łangowski <pawlan@st.amu.edu.pl>
2024-06-02 13:27:13 +0200

    World map UI

--- lib/screens/bottom_navbar/world_map.dart
+++ lib/screens/bottom_navbar/world_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:world_rover/widgets/simple_world_map.dart';
 
 class WorldMapScreen extends StatefulWidget {
@@ -13,11 +14,98 @@ class _WorldMapScreenState extends State<WorldMapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SimpleWorldMap(visitedCountries: visitedCountries),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 8, right: 8, top: 40, bottom: 10),
+      child: Column(
+        children: [
+          SimpleWorldMap(visitedCountries: visitedCountries),
+          const SizedBox(height: 40),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.all(12),
+            height: 220,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(Radius.circular(1)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Statistic",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CircularPercentIndicator(
+                        radius: 60,
+                        lineWidth: 15,
+                        percent: 0.22,
+                        progressColor: Theme.of(context).colorScheme.primary,
+                        center: Text(
+                          "22%",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 26,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "13",
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontSize: 40,
+                            ),
+                          ),
+                          Text(
+                            "Countries",
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: const Text("Add country"),
+                icon: const Icon(Icons.add),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: const Text("Remove country"),
+                icon: const Icon(Icons.remove),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
